f0b40d964a2f1dfe1ff51c7e2d4ad54e4a1bfad9
cghiorzi <Chris.ghiorzi@gmail.com>
2015-09-19 16:15:37 -0400

    Cleaned directory structure

--- src/main/java/tilt/TiltController.java
+++ src/main/java/tilt/TiltController.java
@@ -1,9 +1,6 @@
 package tilt;
 
-import java.io.Writer;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
